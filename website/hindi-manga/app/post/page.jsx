fdf9f5bf57764fa24c847da9708918acb4096497
saikingalgulwad <saiprasadalgulwad@gmail.com>
2024-12-31 01:15:59 -0800

    chnaging url fetch

--- website/hindi-manga/app/post/page.jsx
+++ website/hindi-manga/app/post/page.jsx
@@ -21,7 +21,7 @@ const onSave=()=>{
 }
 const uploadFile=async()=>{
  
-  let req= await fetch("http://localhost:3000/api/userpost",{
+  let req= await fetch("https://hindi-manga.vercel.app/api/userpost",{
      method:"POST",
      body: JSON.stringify({ title:title,number:number,image:image,pdf:pdf}),
    })
