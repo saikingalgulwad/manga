7270229ce14ebfe31af833a81b3071dfc106257c
saikingalgulwad <167662574+saikingalgulwad@users.noreply.github.com>
2025-02-10 20:57:02 +0530

    Update page.jsx

--- hindi-manga/app/page.jsx
+++ hindi-manga/app/page.jsx
@@ -146,18 +146,18 @@ useEffect(() => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
          
         {search?.map((chapter) = (
-          <Link key={chapter._id} href={`/chapter/${chapter._id}`}>
+          <Link key={chapter?._id} href={`/chapter/${chapter?._id}`}>
             <div className="bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transform transition duration-300 cursor-pointer overflow-hidden">
               <Image
-                src={chapter.image} // Rotate through image URLs
-                alt={`${chapter.title} Cover`}
+                src={chapter?.image} // Rotate through image URLs
+                alt={`${chapter?.title} Cover`}
                 width={300}
                 height={200}
                 className="w-full object-cover"
               />
               <div className="p-4">
-                <h2 className="text-2xl text-neonPink">{chapter.title}</h2>
-                <p className="text-gray-400">Chapter {chapter.number}</p>
+                <h2 className="text-2xl text-neonPink">{chapter?.title}</h2>
+                <p className="text-gray-400">Chapter {chapter?.number}</p>
                 <button className="mt-4 px-4 py-2 bg-neonBlue text-darkBg font-bold rounded">
                   Read Now
                 </button>
